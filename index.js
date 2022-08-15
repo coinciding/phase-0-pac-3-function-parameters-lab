d69be97b2e704776021246f540e6ad38703ff559
Justin White <justinnwhitee@gmail.com>
2022-08-15 16:18:13 -0400

    commit tree 845f0a62cc1220843fa03879c3c93a3fefc1c09d

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// function that takes in 1 argument
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+
+// function that takes in 2 arguments
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// functiion that has 1 default parameter
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+
